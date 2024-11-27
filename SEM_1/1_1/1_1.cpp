f202c2b7d96988b60170a7884f7db580ddf7f917
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-11-28 02:43:16 +0500

    Add files via upload

---
+++ SEM_1/1_1/1_1.cpp
@@ -0,0 +1,31 @@
+﻿//Cоздать программу для поиска максимального целого числаиз трех.Числа последовательно вводит пользователь.
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a > b)
+	{
+		if (a > c)
+		{
+			cout << a << endl;
+		}
+		else
+		{
+			cout << c << endl;
+		}
+	}
+	else
+	{
+		if (b > c)
+		{
+			cout << b << endl;
+		}
+		else
+		{
+			cout << c << endl;
+		}
+	}
+	return 0;
+}
