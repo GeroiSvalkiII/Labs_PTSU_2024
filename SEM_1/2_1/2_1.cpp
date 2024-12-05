85f3cbcd91fe50b281ee0a64c6fff31da924f12b
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-12-05 08:21:04 +0500

    Add files via upload

---
+++ SEM_1/2_1/2_1.cpp
@@ -0,0 +1,14 @@
+﻿/*Посчитать сумму N первых натуральных чисел.*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,sum=0;
+	cin >> n;
+	for (int i = n; i > 0; i--)
+	{
+		sum += i;
+	}
+	cout << sum << endl;
+	return 0;
+}
