22636407156c07bfbc363ecaa96d1c67c5e4b2ef
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-12-24 14:15:18 +0500

    Add files via upload

---
+++ SEM_1/2_3/2_3.cpp
@@ -0,0 +1,17 @@
+﻿/*Посчитать сумму
+S = 1 ∗ 2 + 2 ∗ 3 ∗ 4 + 3 ∗ 4 ∗ 5 ∗ 6 + 4 ∗ 5 ∗ 6 ∗ 7 ∗ 8+. . . . до N
+(натурального числа) по порядку*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n, sum = 0;
+	cin >> n;
+	for (int i = n, f = (n-1); f > 0; i--, f--)
+	{
+		sum += f * i;
+	}
+	cout << sum << endl;
+	return 0;
+}
