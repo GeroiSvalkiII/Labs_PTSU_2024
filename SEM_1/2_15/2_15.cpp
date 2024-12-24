7990b85133ac7841fa9b4aa185841bdf02f34639
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-12-25 03:23:08 +0500

    Add files via upload

---
+++ SEM_1/2_15/2_15.cpp
@@ -0,0 +1,21 @@
+﻿/*Дано натуральное число N, проверить, входить ли цифра S в запись этого
+числа.*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,s,f=0;
+	cin >> n >> s;
+	while (n > 0)
+	{
+		if (s == n % 10)
+		{
+			f = 1;
+		}
+		n /= 10;
+	}
+	if (f == 1) cout << "Yes";
+	else cout << "No";
+	return 0;
+}
