e070572e23354de7f131b9a28ebc17b2bce20d47
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-12-25 01:06:09 +0500

    Add files via upload

---
+++ SEM_1/2_9/2_9.cpp
@@ -0,0 +1,79 @@
+﻿/*Отрисовать проекцию пустого куба из звездочек со стороной N.*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int a = n / 2;
+	for (int i = a; i > 0; i--)
+	{
+		cout << "  ";
+	}
+	for (int i = n; i > 0; i--)
+	{
+		cout << " *";
+	}
+	cout << endl;
+	for (int i = a - 1; i > 0; i--)
+	{
+		for (int f = i; f > 0; f--)
+		{
+			cout << "  ";
+		}
+		cout << " *";
+		for (int f = n - 2; f > 0; f--)
+		{
+			cout << "  ";
+		}
+		cout << " *";
+		for (int f = a - i-1; f > 0; f--)
+		{
+			cout << "  ";
+		}
+		cout << " *" << endl;
+	}
+	for (int i = n; i > 0; i--)
+	{
+		cout << " *";
+	}
+	for (int i = a - 1; i > 0; i--)
+	{
+		cout << "  ";
+	}
+	cout << " *" << endl;
+	for (int i = a - 1; i > 0; i--)
+	{
+		cout << " *";
+		for (int f = n - 2; f > 0; f--)
+		{
+			cout << "  ";
+		}
+		cout << " *";
+		for (int f = a - 1; f > 0; f--)
+		{
+			cout << "  ";
+		}
+		cout << " *" << endl;
+	}
+	for (int i = a - 1; i > 0; i--)
+	{
+		cout << " *";
+		for (int f = n - 2; f > 0; f--)
+		{
+			cout << "  ";
+		}
+		cout << " *";
+		for (int f = i - 1; f > 0; f--)
+		{
+			cout << "  ";
+		}
+		cout << " *" << endl;
+	}
+	for (int i = n; i > 0; i--)
+	{
+		cout << " *";
+	}
+	return 0;
+}
