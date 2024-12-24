33a5e23f0710df845235d251399b166cb05bcb60
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-12-24 14:40:36 +0500

    Add files via upload

---
+++ SEM_1/2_7/2_7_1/2_7.1.cpp
@@ -0,0 +1,23 @@
+﻿/*Напечатать квадрат из N звездочек.
+В котором ВСЕГО N звездочек.
+задать требования к минимальному N.*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int n,a;
+	cout << "Введите число n в 2-степени!!!!!!" << endl;
+	cin >> n;
+	a = sqrt(n);
+	for (int i = a; i > 0; i--)
+	{
+		for (int f = a; f > 0; f--)
+		{
+			cout << " *";
+		}
+		cout << endl;
+	}
+	return 0;
+}
