37bf8ba1c945cf24c30c8583fc310ac8897ce3ca
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-12-25 02:45:53 +0500

    Add files via upload

---
+++ SEM_1/2_12/2_12.cpp
@@ -0,0 +1,21 @@
+﻿/*Последовательность из N положительных чисел вводится с клавиатуры.
+Найти сумму максимального и минимального элементов последовательности.*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n, a, min, max;
+	cin >> n;
+	cin >> a;
+	max = a;
+	min = a;
+	for (int i = n; i > 1; i--)
+	{
+		cin >> a;
+		if (a > max) max = a;
+		if (a < min) min = a;
+	}
+	cout << min + max;
+	return 0;
+}
