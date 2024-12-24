5dfd7936a0e0da8573e8304e733def901f18f1d8
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-12-24 15:26:33 +0500

    Add files via upload

---
+++ SEM_1/2_7/2_7_2/2_7.2.cpp
@@ -0,0 +1,21 @@
+﻿/*Напечатать квадрат из N звездочек.
+Заполненный квадрат со стороной N
+задать требования к минимальному N.*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "Введите длину стороны квадрата" << endl;
+	cin >> n;
+	for (int i = n; i > 0; i--)
+	{
+		for(int f = n; f > 0; f--)
+		{
+			cout << " *";
+		}
+		cout << endl;
+	}
+	return 0;
+}
