cca56ef5bb3848bf527f4c1b6b9653a5f6471046
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-12-24 14:16:04 +0500

    Add files via upload

---
+++ SEM_1/2_4/2_4.cpp
@@ -0,0 +1,27 @@
+﻿/*Напечатать в консоли равнобедренный треугольник из
+звездочек ( * ) с вершиной, направленной вверх, и основанием
+N, где N — натуральное нечетное число больше 3.*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int prb = n / 2, fl = n/2+2;
+	for (int i = (n + 1) / 2; i >0 ; i--)
+	{
+		for (int f = prb; f > 0; f--)
+		{
+			cout << " ";
+		}
+		prb -= 1;
+		for (int f = fl - i; f > 0; f--)
+		{
+			cout << "*";
+		}
+		fl += 1;
+		cout << endl;
+	}
+	return 0;
+}
