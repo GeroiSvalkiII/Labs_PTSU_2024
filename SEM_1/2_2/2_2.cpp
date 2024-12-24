1944f112bf2c3c113f967cd91b4593d2b359fe51
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-12-24 14:14:11 +0500

    Add files via upload

---
+++ SEM_1/2_2/2_2.cpp
@@ -0,0 +1,15 @@
+﻿/*Посчитать произведение N первых натуральных чисел.*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n, pr=1;
+	cin >> n;
+	for (int i = n; i > 0; i--)
+	{
+		pr = pr * i;
+	}
+	cout << pr << endl;
+	return 0;
+}
