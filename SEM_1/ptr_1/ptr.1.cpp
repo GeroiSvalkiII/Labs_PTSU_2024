83be3cf255d691573b44a2cf047022beb89cfbab
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-12-25 06:36:30 +0500

    Add files via upload

---
+++ SEM_1/ptr_1/ptr.1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{ 
+	int a, b,c;
+	int *aptr, *bptr;
+	a = 8;
+	b = 9;
+	aptr = &a;
+	bptr = &b;
+	c = *aptr;
+	*aptr = *bptr;
+	*bptr = c;
+	cout << *aptr << endl;
+	cout << *bptr << endl;
+	cout << aptr << endl;
+	cout << bptr << endl;
+	return 0;
+}
