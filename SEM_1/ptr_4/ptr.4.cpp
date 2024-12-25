e441972c2544661fed9ce2962572f153af5c24de
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-12-25 07:52:33 +0500

    Add files via upload

---
+++ SEM_1/ptr_4/ptr.4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, c, *p = &c, ma, mi;
+	cin >> n;
+	cin >> *p;
+	ma = *p;
+	mi = *p;
+	for (int i = 2; i <= n; i++)
+	{
+		cin >> *p;
+		if (*p>ma)
+		{ 
+			ma = *p;
+		}
+		else
+		{
+			if(*p < mi) 
+			{
+				mi = *p;
+			}
+		}
+	}
+	cout << ma << endl;
+	cout << mi << endl;
+	return 0;
+}
