8c7d0b4ce28e1cdbdf548340ea3e3bbe654fa690
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-12-25 05:33:07 +0500

    Add files via upload

---
+++ SEM_1/2_20/2_20.cpp
@@ -0,0 +1,21 @@
+﻿/*Последовательность n элементов задана формулой общего члена
+ai = sin(n +
+i
+n
+). Проверить, если ли среди этих элементов элемент, равный S.*/
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	float n, s,fl=0;
+	cin >> n >> s;
+	for (int i = 1; i < n + 1; i++)
+	{
+		if (sin(n + i / n) == s)fl = 1;
+	}
+	if (fl == 1)cout << "Yes";
+	else cout << "No";
+	return 0;
+}
