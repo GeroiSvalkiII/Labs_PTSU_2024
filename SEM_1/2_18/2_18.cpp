38cd9c4a8e85bc0eb22eab5897ae0d19d0b7b256
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-12-25 05:17:26 +0500

    Add files via upload

---
+++ SEM_1/2_18/2_18.cpp
@@ -0,0 +1,17 @@
+﻿/*Дано натуральное число N. Поменять порядок цифр числа N на обратный.*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,m=0;
+	cin >> n;
+	while (n > 0)
+	{
+		m *= 10;
+		m += n % 10;
+		n = n / 10;
+	}
+	cout << m;
+	return 0;
+}
