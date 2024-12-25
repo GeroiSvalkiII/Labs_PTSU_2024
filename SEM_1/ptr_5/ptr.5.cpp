65d1664c5fe9e185383f33bf71fc30ff26b33e93
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-12-25 07:53:12 +0500

    Add files via upload

---
+++ SEM_1/ptr_5/ptr.5.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a, * p = &a;
+	cin >> a;
+	for (int i = *p-1; i > 0; i--)
+	{
+		*p = *p * i;
+	}
+	cout << *p << endl;
+	return 0;
+}
