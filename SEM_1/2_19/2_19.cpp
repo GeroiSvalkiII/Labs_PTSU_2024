08a367e89608e7cdf08417afadca7c8114b5c324
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-12-25 05:23:17 +0500

    Add files via upload

---
+++ SEM_1/2_19/2_19.cpp
@@ -0,0 +1,16 @@
+﻿/*Вычислить сумму S = 1 + 2 − 3 + 4 + 5 − 6 + 7 + 8 − 9. . . до N слагаемых.*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n, sum=0;
+	cin >> n;
+	for (int i = 1; i < n+1; i++)
+	{
+		if (i % 3 == 0)  sum -= i;
+		else sum += i;
+	}
+	cout << sum;
+	return 0;
+}
