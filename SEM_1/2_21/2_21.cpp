2eeb038350f523861e9247ef2d4153c60da579a5
Кобяков Егор Александрович <kolakovegor@gmail.com>
2024-12-25 05:41:36 +0500

    Add files via upload

---
+++ SEM_1/2_21/2_21.cpp
@@ -0,0 +1,20 @@
+﻿/*Числа вводятся с клавиатуры до тех пор, пока не встретится число 0 (0 —
+признак окончания ввода). Проверить, упорядочены ли числа по возрастанию.*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,a,fl=0;
+	cin >> n;
+	a = n;
+	while (n != 0)
+	{
+		cin >> n;
+		if ((a > n) && (n!=0))fl = 1;
+		a = n;
+	}
+	if (fl == 0)cout << "Yes";
+	else cout << "No";
+	return 0;
+}
